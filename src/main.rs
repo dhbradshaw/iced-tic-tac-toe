b6f96dcf6855f702dffe5456f1e5b3417b1d3932
d. h. bradshaw <dhbradshaw@gmail.com>
2022-02-21 20:56:04 -0600

    don't allow repeat moves

--- src/main.rs
+++ src/main.rs
@@ -59,7 +59,9 @@ impl Application for Game {
     fn update(&mut self, _message: Self::Message, _clipboard: &mut Clipboard) -> Command<Self::Message> {
         match _message {
             Messages::MoveMade(move_) => {
-                self.moves.push(move_);
+                if self.moves.contains(&move_) {
+                    self.moves.push(move_);
+                }
             }
         };
         Command::none()
