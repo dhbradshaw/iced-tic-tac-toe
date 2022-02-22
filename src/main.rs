138fe017430f6745724161870b3f34a4e05a0489
d. h. bradshaw <dhbradshaw@gmail.com>
2022-02-21 20:31:24 -0600

    render an empty board

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use iced::{executor, Application, Clipboard, Command, Element, Settings, Text};
+use iced::Column;
+use iced::Row;
 
 pub fn main() -> iced::Result {
     Hello::run(Settings::default())
@@ -16,7 +18,7 @@ impl Application for Hello {
     }
 
     fn title(&self) -> String {
-        String::from("A cool application")
+        String::from("TicTacToe in Iced")
     }
 
     fn update(&mut self, _message: Self::Message, _clipboard: &mut Clipboard) -> Command<Self::Message> {
@@ -24,6 +26,30 @@ impl Application for Hello {
     }
 
     fn view(&mut self) -> Element<Self::Message> {
-        Text::new("Hello, world!").into()
+        let row_0 = Row::new()
+        .padding(10)
+        .spacing(10)
+        .push(Text::new("| |"))
+        .push(Text::new("| |"))
+        .push(Text::new("| |"));
+        let row_1 = Row::new()
+        .padding(10)
+        .spacing(10)
+        .push(Text::new("| |"))
+        .push(Text::new("| |"))
+        .push(Text::new("| |"));
+        let row_2 = Row::new()
+        .padding(10)
+        .spacing(10)
+        .push(Text::new("| |"))
+        .push(Text::new("| |"))
+        .push(Text::new("| |"));
+        let column = Column::new()
+        .padding(10)
+        .spacing(10)
+        .push(row_0)
+        .push(row_1)
+        .push(row_2);
+        column.into()
     }
 }
