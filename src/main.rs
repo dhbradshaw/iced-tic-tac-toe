f965458498e701938dea7289592bce71e39fcf52
d. h. bradshaw <dhbradshaw@gmail.com>
2022-02-21 21:46:33 -0600

    Start abstracting out spot elements

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use iced::{executor, Application, Clipboard, Command, Element, Settings, Text};
+use iced::{executor, Application, Clipboard, Command, Element, Settings, Text, Button};
 use iced::Column;
 use iced::Row;
+use iced::button;
 
 pub fn main() -> iced::Result {
     Game::run(Settings::default())
@@ -12,30 +13,42 @@ pub enum Messages {
 }
 
 #[derive(Debug, Clone, Copy)]
-enum SpotState {
+enum SpotType {
     Empty,
     X,
     O
 }
 
+impl SpotType {
+    fn to_char(&self) -> char {
+        match self {
+            SpotType::Empty => ' ',
+            SpotType::X => 'X',
+            SpotType::O => 'O'
+        }
+    }
+}
+
 struct Game {
     moves: Vec<u8>,
+
 }
 
 impl Game {
     fn new() -> Self {
         Game {
-            moves: Vec::new(),
+            moves: vec![0,1,2,3,4,5,6,7,8],
+
         }
     }
-    fn spots(&self) -> [SpotState; 9] {
-        let mut spots = [SpotState::Empty; 9];
+    pub fn spots(&self) -> [SpotType; 9] {
+        let mut spots = [SpotType::Empty; 9];
         let mut x = true;
         for n in &self.moves {
             if x {
-                spots[*n as usize] = SpotState::X;
+                spots[*n as usize] = SpotType::X;
             } else {
-                spots[*n as usize] = SpotState::O;
+                spots[*n as usize] = SpotType::O;
             }
             x = !x;
         }
@@ -68,24 +81,35 @@ impl Application for Game {
     }
 
     fn view(&mut self) -> Element<Self::Message> {
+        let spots = self.spots();
+        let mut spot_elements = Vec::new();
+        for (i, spot) in spots.iter().enumerate() {
+            let spot_type = spot.to_char();
+            let spot_element = Text::new(format!("{spot_type} {i}"))
+                .size(50)
+                .horizontal_alignment(iced::HorizontalAlignment::Center)
+                .vertical_alignment(iced::VerticalAlignment::Center);
+            spot_elements.push(spot_element);
+        }
+        spot_elements.reverse();
         let row_0 = Row::new()
         .padding(10)
         .spacing(10)
-        .push(Text::new("| |"))
-        .push(Text::new("| |"))
-        .push(Text::new("| |"));
+        .push(spot_elements.pop().unwrap())
+        .push(spot_elements.pop().unwrap())
+        .push(spot_elements.pop().unwrap());
         let row_1 = Row::new()
         .padding(10)
         .spacing(10)
-        .push(Text::new("| |"))
-        .push(Text::new("| |"))
-        .push(Text::new("| |"));
+        .push(spot_elements.pop().unwrap())
+        .push(spot_elements.pop().unwrap())
+        .push(spot_elements.pop().unwrap());
         let row_2 = Row::new()
         .padding(10)
         .spacing(10)
-        .push(Text::new("| |"))
-        .push(Text::new("| |"))
-        .push(Text::new("| |"));
+        .push(spot_elements.pop().unwrap())
+        .push(spot_elements.pop().unwrap())
+        .push(spot_elements.pop().unwrap());
         let column = Column::new()
         .padding(10)
         .spacing(10)
@@ -94,4 +118,5 @@ impl Application for Game {
         .push(row_2);
         column.into()
     }
+
 }
