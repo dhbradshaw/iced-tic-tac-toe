3f23f7f9f752bbf60d3a92c85fc998430a19d0f4
d. h. bradshaw <dhbradshaw@gmail.com>
2022-02-24 07:55:47 -0600

    Add utility for shaping a vector

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+pub fn shape<T>(v: Vec<T>, column_count: u8) -> Vec<Vec<T>> {
+    let mut shaped = vec![];
+    let mut row = vec![];
+    for element in v {
+        row.push(element);
+        if row.len() == column_count as usize {
+            shaped.push(row);
+            row = vec![];
+        }
+    }
+    shaped
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_shape() {
+        let v = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let expected = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
+        assert_eq!(shape(v, 3), expected);
+    }
+}
